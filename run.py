24035e59c2f744e5fbd4980c6af83a516e42f8d0
Denis Rono <denisrono406@gmail.com>
2026-06-05 21:49:54 +0300

    Fix Flask-SocketIO deployment

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ app = create_app(os.environ.get('FLASK_ENV', 'development'))
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=False)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)), debug=False, allow_unsafe_werkzeug=True)
